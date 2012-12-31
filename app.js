0d4a0ca3f2f2d339beb16b5502e12bf5bdd4d6d1
Ofer Moshaioff <ofer.moshaioff@gmail.com>
2012-12-31 22:41:21 +0200

    initial commit

---
+++ app.js
@@ -0,0 +1,44 @@
+var	express = require('express'),
+	request = require('request');
+
+var app = express()
+  , server = require('http').createServer(app);
+  
+var port = process.env.PORT || 8888;
+server.listen(port);
+var ip = process.env.IP || 'http://localhost';
+
+console.log('Starting Server at address: ' + ip + ":" + port);
+
+app.configure(function() {
+	app.use(express.logger());
+	app.use(express.bodyParser());
+});
+
+
+app.get('/', function(req, res) {
+	return get_kzradio_live_or_vod_url(function(err, url) {
+		res.writeHead(200, { 'Content-Type': 'application/json' });
+		if (url) {
+			res.write(JSON.stringify({url: url}));
+		}
+		res.end();
+	});
+});
+
+function get_kzradio_live_or_vod_url(callback) {
+	request('http://kzradio.net', function(error, response, body) {
+		if (!error && response.statusCode == 200) {
+			var regex = new RegExp('/.+show-live-stream-url.+(http.+)<\/span>.+/');
+			var url = regex.exec(body)[1];			
+			console.log(url);
+			return callback(null, url);
+		} else {
+			return (error, null);
+		}
+	});
+}
+	
+
+
+	
